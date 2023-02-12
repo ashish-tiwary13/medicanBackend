fa904d7460af464b457cb3363312fc8502d21a06
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-02-13 04:52:58 +0530

    try

--- index.js
+++ index.js
@@ -134,17 +134,17 @@ app.post('/scrape', async (req, res) => {
 
 // deployment
 
-__dirname = path.resolve();
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static(path.join(__dirname,"/frontend/build")));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
-  });
-}else{
+// __dirname = path.resolve();
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static(path.join(__dirname,"/frontend/build")));
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
+//   });
+// }else{
   app.get("/",(req,res)=>{
     res.send("API is running..");
   });
-}
+// }
 //
 
 
