41af610e40240239114da4bdb3a5326ff301b619
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-02-13 21:40:28 +0530

    changes on puppeteer

--- onemg.js
+++ onemg.js
@@ -1,7 +1,18 @@
 const puppeteer = require("puppeteer");
 
 const two =  async(search) =>{
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        // "--disable-dev-shm-usage",
+        // "--disable-accelerated-2d-canvas",
+        // "--no-first-run",
+        // "--no-zygote",
+        // "--single-process", // <- this one doesn't works in Windows
+        // "--disable-gpu",
+      ]
+    });
     const page = await browser.newPage(); 
     await page.goto(`https://www.1mg.com/search/all?name=${search}`);
     
