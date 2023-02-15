7eadc734b6b558261d237d290c8ab8761d698866
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-02-15 19:39:40 +0530

    final form, not for deployement!!!!!

--- index.js
+++ index.js
@@ -26,101 +26,100 @@ app.post('/scrape', async (req, res) => {
   }
 
   try {
-    const json1 = await pharmaApollo.getApollo(search);
+    // const json1 = await pharmaApollo.getApollo(search);
     // const json2 = await pharmaOneMg.getOneMg(search);
     // const json3 = await PharmaPharmeasy.getPharmeasy(search);
     // const json4 = await pharmaNetmeds.getNetmeds(search);
 
-//     const [json1,json2,json3,json4] = await Promise.all([pharmaApollo.getApollo(search),pharmaOneMg.getOneMg(search),PharmaPharmeasy.getPharmeasy(search),pharmaNetmeds.getNetmeds(search)]);
+    const [json1,json2,json3,json4] = await Promise.all([pharmaApollo.getApollo(search),pharmaOneMg.getOneMg(search),PharmaPharmeasy.getPharmeasy(search),pharmaNetmeds.getNetmeds(search)]);
 
 
 // //
 
-//         const medicanApi1=[];
-//         const medicanApi2=[];
-//         const medicanApi3=[]; 
-//         let nameScore=-1;
-//         let mrpScore=-1;
-//         let x=0;
-//         for (let i = 0; i < 30; i++) {
-//             const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",image: "" ,apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: ""};
-//             for (let j = 0; j < 30; j++) {
-//                 nameScore = fuzz.token_set_ratio(json1[i].name, json2[j].name);
-//                 mrpScore = fuzz.token_set_ratio(json1[i].mrp, json2[j].mrp);
-//                 if(nameScore>50 && mrpScore==100){
-//                     // console.log(json1[i].name +"||"+json2[j].name+"||"+nameScore+"||"+mrpScore);
-//                     data.name = json1[i].name;
-//                     data.mrp = json1[i].mrp;
-//                     data.apolloPrice = json1[i].actualPrice;
-//                     data.oneMgPrice = json2[j].actualPrice;
-//                     data.image = json1[i].image;
-//                     data.apolloLink = json1[i].link;
-//                     data.oneMgLink = json2[j].link;
-//                 }else{
-//                     x++;
-//                 }
-//             }
-//         medicanApi1.push(data);
-//         }
+        const medicanApi1=[];
+        const medicanApi2=[];
+        const medicanApi3=[]; 
+        let nameScore=-1;
+        let mrpScore=-1;
+        let x=0;
+        for (let i = 0; i < 30; i++) {
+            const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",image: "" ,apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: ""};
+            for (let j = 0; j < 30; j++) {
+                nameScore = fuzz.token_set_ratio(json1[i].name, json2[j].name);
+                mrpScore = fuzz.token_set_ratio(json1[i].mrp, json2[j].mrp);
+                if(nameScore>50 && mrpScore==100){
+                    // console.log(json1[i].name +"||"+json2[j].name+"||"+nameScore+"||"+mrpScore);
+                    data.name = json1[i].name;
+                    data.mrp = json1[i].mrp;
+                    data.apolloPrice = json1[i].actualPrice;
+                    data.oneMgPrice = json2[j].actualPrice;
+                    data.image = json1[i].image;
+                    data.apolloLink = json1[i].link;
+                    data.oneMgLink = json2[j].link;
+                }else{
+                    x++;
+                }
+            }
+        medicanApi1.push(data);
+        }
         
-//         // between medicanAp1 and pharmeasy
-//         for (let i = 0; i < 30; i++) {
-//             const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",image: ""  ,apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: ""};
-//             for (let j = 0; j < 30; j++) {
-//                 nameScore = fuzz.token_set_ratio(medicanApi1[i].name, json3[j].name);
-//                 mrpScore = fuzz.token_set_ratio(medicanApi1[i].mrp, json3[j].mrp);
-//                 data.name = medicanApi1[i].name;
-//                 data.mrp = medicanApi1[i].mrp;
-//                 data.apolloPrice = medicanApi1[i].apolloPrice;
-//                 data.oneMgPrice = medicanApi1[i].oneMgPrice;
-//                 data.apolloLink = medicanApi1[i].apolloLink;
-//                 data.oneMgLink = medicanApi1[i].oneMgLink;
-//                 data.image = medicanApi1[i].image;
-//                 if(nameScore>50 && mrpScore==100){
-//                     // console.log(medicanApi1[i].name +"||"+json3[j].name+"||"+nameScore+"||"+mrpScore);
-//                     data.pharmeasyPrice = json3[j].actualPrice;
-//                     data.pharmeasyLink = json3[j].link;
-//                     break;
-//                 }else{
-//                     data.pharmeasyPrice =0;
+        // between medicanAp1 and pharmeasy
+        for (let i = 0; i < 30; i++) {
+            const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",image: ""  ,apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: ""};
+            for (let j = 0; j < 30; j++) {
+                nameScore = fuzz.token_set_ratio(medicanApi1[i].name, json3[j].name);
+                mrpScore = fuzz.token_set_ratio(medicanApi1[i].mrp, json3[j].mrp);
+                data.name = medicanApi1[i].name;
+                data.mrp = medicanApi1[i].mrp;
+                data.apolloPrice = medicanApi1[i].apolloPrice;
+                data.oneMgPrice = medicanApi1[i].oneMgPrice;
+                data.apolloLink = medicanApi1[i].apolloLink;
+                data.oneMgLink = medicanApi1[i].oneMgLink;
+                data.image = medicanApi1[i].image;
+                if(nameScore>50 && mrpScore==100){
+                    // console.log(medicanApi1[i].name +"||"+json3[j].name+"||"+nameScore+"||"+mrpScore);
+                    data.pharmeasyPrice = json3[j].actualPrice;
+                    data.pharmeasyLink = json3[j].link;
+                    break;
+                }else{
+                    data.pharmeasyPrice =0;
                     
-//                     x++;
-//                 }
-//             }
-//             medicanApi2.push(data);
-//         }
+                    x++;
+                }
+            }
+            medicanApi2.push(data);
+        }
         
-//         // between medicanApi2 and netmeds
-//         for (let i = 0; i < 30; i++) {
-//             const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: "",image: ""  };
-//             for (let j = 0; j < 30; j++) {
-//                 nameScore = fuzz.token_set_ratio(medicanApi2[i].name, json4[j].name);
-//                 mrpScore = fuzz.token_set_ratio(medicanApi2[i].mrp, json4[j].mrp);
-//                 data.name = medicanApi2[i].name;
-//                 data.mrp = medicanApi2[i].mrp;
-//                 data.apolloPrice = medicanApi2[i].apolloPrice;
-//                 data.oneMgPrice = medicanApi2[i].oneMgPrice;
-//                 data.pharmeasyPrice = medicanApi2[i].pharmeasyPrice;
-//                 data.apolloLink = medicanApi2[i].apolloLink;
-//                 data.oneMgLink = medicanApi2[i].oneMgLink;
-//                 data.pharmeasyLink = medicanApi2[i].pharmeasyLink;
-//                 data.image = medicanApi2[i].image;
-//                 if(nameScore>50 && mrpScore==100){
-//                     // console.log(medicanApi2[i].name +"||"+json4[j].name+"||"+nameScore+"||"+mrpScore);
-//                     data.netmedsPrice = json4[j].actualPrice;
-//                     data.netmedsLink = json4[j].link;
-//                     break;
-//                 }else{        
-//             data.netmedsPrice =0;
-//             x++;
-//                 }
-//             }
-//             medicanApi3.push(data);
-//         }
-//         const filterData = medicanApi3.filter((item) => item.mrp !== '');
-        // console.log(filterData);
-        // res.send(filterData);
-        res.send(json1);
+        // between medicanApi2 and netmeds
+        for (let i = 0; i < 30; i++) {
+            const data = { index: i, name:"",mrp:"" ,apolloPrice:"",oneMgPrice:"",pharmeasyPrice:"", netmedsPrice: "",apolloLink:"",oneMgLink:"",pharmeasyLink:"", netmedsLink: "",image: ""  };
+            for (let j = 0; j < 30; j++) {
+                nameScore = fuzz.token_set_ratio(medicanApi2[i].name, json4[j].name);
+                mrpScore = fuzz.token_set_ratio(medicanApi2[i].mrp, json4[j].mrp);
+                data.name = medicanApi2[i].name;
+                data.mrp = medicanApi2[i].mrp;
+                data.apolloPrice = medicanApi2[i].apolloPrice;
+                data.oneMgPrice = medicanApi2[i].oneMgPrice;
+                data.pharmeasyPrice = medicanApi2[i].pharmeasyPrice;
+                data.apolloLink = medicanApi2[i].apolloLink;
+                data.oneMgLink = medicanApi2[i].oneMgLink;
+                data.pharmeasyLink = medicanApi2[i].pharmeasyLink;
+                data.image = medicanApi2[i].image;
+                if(nameScore>50 && mrpScore==100){
+                    // console.log(medicanApi2[i].name +"||"+json4[j].name+"||"+nameScore+"||"+mrpScore);
+                    data.netmedsPrice = json4[j].actualPrice;
+                    data.netmedsLink = json4[j].link;
+                    break;
+                }else{        
+            data.netmedsPrice =0;
+            x++;
+                }
+            }
+            medicanApi3.push(data);
+        }
+        const filterData = medicanApi3.filter((item) => item.mrp !== '');
+        console.log(filterData);
+        res.send(filterData);
 
 
 //
@@ -132,19 +131,11 @@ app.post('/scrape', async (req, res) => {
 
 
 
-// deployment
 
-// __dirname = path.resolve();
-// if(process.env.NODE_ENV ==='production'){
-//   app.use(express.static(path.join(__dirname,"/frontend/build")));
-//   app.get('*',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
-//   });
-// }else{
+
   app.get("/",(req,res)=>{
     res.send("API is running..");
   });
-// }
 //
 
 
